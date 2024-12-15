40a251eb465f74909af57cfb81eaf3f604f94d80
Courtney Macgregor <cjoss90@hotmail.com>
2024-12-15 14:45:26 +1000

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(arr,name) {
+    return arr.filter(drivers => drivers.toLowerCase() === name.toLowerCase());
+}
+
+function fuzzyMatch(arr,name) {
+    return arr.filter((drivers) => drivers.toLowerCase().startsWith(name.toLowerCase()));
+}
+
+function matchName(arr,name) {
+    return arr.filter(drivers => drivers.name.toLowerCase() === name.toLowerCase());
+}
